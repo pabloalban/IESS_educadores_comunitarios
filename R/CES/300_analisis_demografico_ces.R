c7ed4584bb7baea576b8ab5fb0b49eabcb3968e1
pabloalban <pablo.alban@iess.gob.ec>
2023-03-01 15:18:15 -0500

    Borrado

--- R/CES/300_analisis_demografico_ces.R
+++
@@ -1,8 +0,0 @@
-# Suavisamiento de tablas de siniestralidad de Cesantía---------------------------------------------
-source( 'R/ces/200_estadistica_motivos_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/201_ajuste_cesantes_normal_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/202_ajuste_jubilados_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/203_ajuste_debito_automatico_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/204_ajuste_pv_desempleo_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/205_ajuste_fondos_saldos_disponibles_ini_ces.R', encoding = 'UTF-8', echo = FALSE )
-source( 'R/ces/206_ajuste_f_i_ces.R', encoding = 'UTF-8', echo = FALSE )
